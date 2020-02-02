eef6ca6494c7fc072e644b9cb2e19259c590bfb8
utsavmajhi <utsavmajhi@gmail.com>
2020-02-02 18:19:23 +0530

    fixed change cover featue and added crop feature with editing tools

--- app/src/main/java/com/example/t1/MyBooksModels/ownbookAdapter.java
+++ app/src/main/java/com/example/t1/MyBooksModels/ownbookAdapter.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.t1.R;
 import com.example.t1.particularbkdetails;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -36,6 +38,17 @@ public class ownbookAdapter extends RecyclerView.Adapter<ownbookAdapter.ownbookV
         holder.mbkname.setText(mallbookitemlist.get(position).getBookname());
         holder.mbkauthor.setText(mallbookitemlist.get(position).getAuthor());
         holder.mbkisbn.setText(mallbookitemlist.get(position).getIsbn());
+        String path=mallbookitemlist.get(position).imageurl;
+        if(!path.isEmpty())
+        {
+            Picasso.with(mContext).load(mallbookitemlist.get(position).imageurl)
+                    .placeholder(R.drawable.bkimage).into(holder.mbkcover);
+        }
+       /* Picasso.with(mContext).load(mallbookitemlist.get(position).imageurl)
+                .placeholder(R.drawable.bkimage).into(holder.mbkcover);
+
+        */
+
 
         //click item listener
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -71,7 +84,7 @@ public class ownbookAdapter extends RecyclerView.Adapter<ownbookAdapter.ownbookV
         public TextView mbkname;
         public TextView mbkauthor;
         public TextView mbkisbn;
-
+        public ImageView mbkcover;
 
         public ownbookView(@NonNull View itemView) {
             super(itemView);
@@ -79,6 +92,7 @@ public class ownbookAdapter extends RecyclerView.Adapter<ownbookAdapter.ownbookV
             mbkname=itemView.findViewById(R.id.mybksinglename);
             mbkauthor=itemView.findViewById(R.id.mybksingleauthor);
             mbkisbn=itemView.findViewById(R.id.mybksingleisbn);
+            mbkcover=itemView.findViewById(R.id.itembkcover);
         }
     }
 
--- app/src/main/java/com/example/t1/particularbkdetails.java
+++ app/src/main/java/com/example/t1/particularbkdetails.java
@@ -2,14 +2,17 @@ package com.example.t1;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.t1.RetrofitApis.ApiInterface;
@@ -21,7 +24,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.Objects;
 
 import es.dmoral.toasty.Toasty;
 import retrofit2.Call;
@@ -32,6 +38,7 @@ public class particularbkdetails extends AppCompatActivity {
     private Toolbar mtoolbar;
     private StorageReference mImageStorage;
     private  static final int GALLERY_PIC=1;
+    private ImageView bkcoverimg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,33 +47,109 @@ public class particularbkdetails extends AppCompatActivity {
         Intent intent=getIntent();
         String bkisbn=intent.getStringArrayExtra("ID_EXTRA")[0];
         mImageStorage= FirebaseStorage.getInstance().getReference();
+        bkcoverimg=findViewById(R.id.bkcoverimage);
         String bkname=intent.getStringArrayExtra("ID_EXTRA")[1];
         String bkauthor=intent.getStringArrayExtra("ID_EXTRA")[2];
         String bkimgurl=intent.getStringArrayExtra("ID_EXTRA")[3];
         setSupportActionBar(mtoolbar);
         getSupportActionBar().setTitle(bkname);
+        if(!bkimgurl.isEmpty())
+        {
+            Picasso.with(particularbkdetails.this).load(bkimgurl).into(bkcoverimg);
+        }
+
     }
 
     public void changecoverclick(View view) {
         Intent galleryIntent=new Intent();
         galleryIntent.setType("image/*");
         galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-
-        startActivityForResult(Intent.createChooser(galleryIntent,"SELECT IMAGE"),GALLERY_PIC);
+        CropImage.activity()
+                .setAspectRatio(3,4)
+                .start(this);
+        //CropImage.ActivityResult result = CropImage.getActivityResult(data);
+        //startActivityForResult(Intent.createChooser(galleryIntent,"SELECT IMAGE"),GALLERY_PIC);
 
 
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==GALLERY_PIC&&resultCode==RESULT_OK)
-        {
-            Uri imageUri=data.getData();
 
-            Uri resultUri = imageUri;
-            final StorageReference filepath=mImageStorage.child("BookCovers").child("my"+".jpg");
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                Intent intent=getIntent();
+                String bkisbn=intent.getStringArrayExtra("ID_EXTRA")[0];
+                final StorageReference filepath=mImageStorage.child("BookCovers").child(bkisbn+".jpg");
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful())
+                        {
+                            filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    //Got download url now put it in blockchain
+                                    final String download_url=uri.toString();
+                                    SharedPreferences sharedPreferences=getSharedPreferences("Secrets",MODE_PRIVATE);
+                                    String currenttoken=sharedPreferences.getString("token","");
+                                    //backend starts
+                                    ApiInterface apiInterface= RetrofitClient.getClient().create(ApiInterface.class);
+                                    Intent intent=getIntent();
+                                    String bkisbn=intent.getStringArrayExtra("ID_EXTRA")[0];
+                                    Sendcoverimgurlformat senddet=new Sendcoverimgurlformat(bkisbn,download_url);
+                                    Call<Getregisformat> c=apiInterface.changecover(senddet,currenttoken);
+                                    c.enqueue(new Callback<Getregisformat>() {
+                                        @Override
+                                        public void onResponse(Call<Getregisformat> call, Response<Getregisformat> response) {
+                                            if(response.isSuccessful())
+                                            {
+                                                Toasty.success(particularbkdetails.this,"Successfully Uploaded Cover Image",Toasty.LENGTH_SHORT).show();
+
+                                                Picasso.with(particularbkdetails.this).load(download_url).into(bkcoverimg);
+                                            }
+                                            else
+                                            {
+                                                Toasty.error(particularbkdetails.this,response.body().getMsg(),Toasty.LENGTH_SHORT).show();
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onFailure(Call<Getregisformat> call, Throwable t) {
+                                            Toasty.error(particularbkdetails.this,t.getMessage(),Toasty.LENGTH_SHORT).show();
+                                        }
+                                    });
+
+                                }
+                            });
+                        }
+                    }
+                });
+
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+        /*if(requestCode==GALLERY_PIC&&resultCode==RESULT_OK)
+        {
+
+
+            Uri imageUri= data.getData();
+
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            Toast.makeText(this, "Entering", Toast.LENGTH_SHORT).show();
+            Uri resultUri = result.getUri();
+
+            Intent intent=getIntent();
+            String bkisbn=intent.getStringArrayExtra("ID_EXTRA")[0];
+            final StorageReference filepath=mImageStorage.child("BookCovers").child(bkisbn+".jpg");
             filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
@@ -76,7 +159,7 @@ public class particularbkdetails extends AppCompatActivity {
                             @Override
                             public void onSuccess(Uri uri) {
                                 //Got download url now put it in blockchain
-                                String download_url=uri.toString();
+                                final String download_url=uri.toString();
                                 SharedPreferences sharedPreferences=getSharedPreferences("Secrets",MODE_PRIVATE);
                                 String currenttoken=sharedPreferences.getString("token","");
                                 //backend starts
@@ -92,6 +175,7 @@ public class particularbkdetails extends AppCompatActivity {
                                         {
                                             Toasty.success(particularbkdetails.this,"Successfully Uploaded Cover Image",Toasty.LENGTH_SHORT).show();
 
+                                            Picasso.with(particularbkdetails.this).load(download_url).into(bkcoverimg);
                                         }
                                         else
                                         {
@@ -110,10 +194,19 @@ public class particularbkdetails extends AppCompatActivity {
                     }
                 }
             });
+            if (resultCode == RESULT_OK) {
+
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
 
 
 
-        }
+
+
+
+        }*/
 
     }
 }
